7931f5828d053f274982fd47f87212c66f9c37d4
cdac <cdac@DESKTOP-SS5H7HT>
2019-02-01 11:07:17 -0800

    Committed sample project for day1

---
+++ day1/day1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day1 sample project");
+        }
+    }
+}
