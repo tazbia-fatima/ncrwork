7ad292d4f14bb22b5e78d0646e20032a04ce45e0
tazbia-fatima <tazzbiafatima@gmail.com>
2019-02-01 15:39:58 -0800

    Taking data from user committed

---
+++ Calculator/Calculator/Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0, b = 0;
+	cout << "Enter 2 nos" << endl;
+	cin >> a >> b;
+	cout << "The value of a is" << a << "The value of b is" << b << endl;
+
+}
