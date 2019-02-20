4b35c56e1253636a3b1beeacf6d8f969c5c11e25
tazbia-fatima <tazzbiafatima@gmail.com>
2019-02-21 03:00:08 +0530

    done

---
+++ C/assignment4/question6-reverse/rev.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void rev(char *);
+int main() {
+	char *str = NULL;
+	int l;
+	str = (char *)malloc(50 * sizeof(char));
+	printf("Enter the string\n");
+	scanf("%[^\n]s", str);
+	//l = strlen(str);
+	printf("%s", str); 
+	rev(str);
+	//printf("\nThe reversed string is %s", str);
+	scanf("%[^\n]s", str);
+	getchar();
+	return 0;
+}
+void rev(char *str) {
+	//char a[50];
+	//int i = 0;
+	if (*str) {
+		rev(str + 1);
+		printf("%c", *str);
+	}
+	//return a;
+}
---
+++ C/assignment4/question7-sortstring/main.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void main()
+{
+
+	char **p;
+	char *temp;
+	char s[50];
+	int n, i, j, len;
+	printf("Enter how many strings\n");
+	scanf("%d", &n);
+
+	p = (char**)malloc(n * sizeof(char*));
+
+	printf("Enter the strings\n");
+	for (i = 0; i < n; i++)
+	{
+		scanf("%s", s);
+		len = strlen(s);
+		p[i] = (char*)malloc(len * sizeof(char));
+		strcpy(p[i], s);
+	}
+	//temp = p[0][0];
+	//printf("First char is %c", temp);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - i; j++)
+		{
+
+			if (p[j][0] > p[j + 1][0])
+			{
+
+				temp = p[i];
+				p[i] = p[j];
+				p[j] = temp;
+
+			}
+		}
+
+	}
+
+	printf("After sorting\n");
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s\n", p[i]);
+	}
+	scanf("%d", n);
+}
