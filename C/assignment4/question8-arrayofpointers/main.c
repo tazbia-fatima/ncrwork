4582da7aba3536dd72aa1fc17e3b678c43c3dc8d
tazbia-fatima <tazzbiafatima@gmail.com>
2019-02-21 03:56:39 +0530

    quest8

---
+++ C/assignment4/question8-arrayofpointers/main.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void main()
+{
+	char **arr;
+	char* s[50], temp[50];
+	int n, i, j, l;
+	printf("enter the number of strings\n");
+	scanf("%d", &n);
+	arr = (char **)malloc(n * sizeof(char *));
+	printf("enter %d strings\n", n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("\n%[^\n]s", s);
+		l = strlen(s) + 1;
+		arr[i] = (char *)malloc((l) * sizeof(char));
+		strcpy(arr[i], s);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", arr[i]);
+
+	}
+	scanf("%d", &i);
+	getchar();
+	
+
+}
