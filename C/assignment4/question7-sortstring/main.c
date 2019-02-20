9d128733a8e6ee2ccef4cb69edbdde5d6bca4dae
tazbia-fatima <tazzbiafatima@gmail.com>
2019-02-21 03:48:05 +0530

    quest7

--- C/assignment4/question7-sortstring/main.c
+++ C/assignment4/question7-sortstring/main.c
@@ -23,23 +23,30 @@ void main()
 		p[i] = (char*)malloc(len * sizeof(char));
 		strcpy(p[i], s);
 	}
+	printf("BEFERO\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%s\n", p[i]);
+	}
 	//temp = p[0][0];
 	//printf("First char is %c", temp);
 	for (i = 0; i < n; i++)
 	{
+		printf("1st for");
 		for (j = 0; j < n - i; j++)
 		{
+			printf("\n2st for");
 
-			if (p[j][0] > p[j + 1][0])
+			if (/*p[j][0]*/(*(*(p+j)+0)) > (*(*(p+j+1)+0)) /*p[j + 1][0]*/)
 			{
-
-				temp = p[i];
-				p[i] = p[j];
-				p[j] = temp;
+				temp = *(p+i);
+				*(p+i)= *(p+j);
+				*(p + j) = temp;
 
 			}
+			printf("End of 2nd for");
 		}
-
+		printf("end of 1st for");
 	}
 
 	printf("After sorting\n");
@@ -49,4 +56,5 @@ void main()
 		printf("%s\n", p[i]);
 	}
 	scanf("%d", n);
+	getchar();
 }
